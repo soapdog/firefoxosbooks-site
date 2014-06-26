5bc84af195d5a07c71f74d57ecd762915e31f2ae
Andre Alves Garzia <contato@andregarzia.com>
2014-06-26 17:04:21 -0300

    initial commit

--- data/data.js
+++ data/data.js
@@ -30,6 +30,17 @@ var data = {
       "description": "In this book three standard game technologies are examined, compared, and contrasted: CSS Sprites, Canvas, and SVG (Scalable Vector Graphics). You will see examples using each of the three to move graphics, detect collisions, and more. Variations within Canvas and SVG are explored as well. You'll also see examples for phone-specific technologies such as Touch, Vibration, Device Orientation, and Screen Orientation. You'll also be shown how to upgrade your Firefox OS phone, how to use the Simulator if you don't have a phone, and how to use the App Manager to put your apps on phones.<br><br>This book assumes you know the basics of HTML5, CSS, and JavaScript, and that you are interested in making games. Concepts and code are explained carefully along the way and newer options like CSS Sprites and SVG are covered extensively, as well as current JavaScript and HTML5 tips and tricks. In fact, except for the phone-specific chapters, you can use these techniques to make HTML5 games for other platforms. All the code is straight HTML5, CSS, SVG, and JavaScript with no libraries, no jQuery, nothing but pure code. The focus is 100% on Firefox OS games and I hope you will be inspired to create games for this fast-growing platform.<br><br>The book is written in an informal, friendly style that invites the reader to come along and explore the fun that is Firefox OS! There isn't just one way to write a game, there are a great many, and it is hoped that this book can inform and inspire you to make your own games."
     },
     {
+      "title": "Beyond Web and Firefox OS - GAIA FROM ABOVE",
+      "author": "看見Gaia",
+      "twitter": "#",
+      "status": "Published",
+      "url": "https://leanpub.com/gaiafromabove",
+      "cover": "https://s3.amazonaws.com/titlepages.leanpub.com/gaiafromabove/large?1402367604",
+      "price": "$4.99 - $14.99",
+      "license": "Commercial",
+      "description": "The book is provide an overview of the javascript written OS: FirefoxOS front end - gaia. We'd like to document and findout how mozilla and the community design and achieve this ongoing great goal. And try to learn things from their design decisions. The book help developers understand how B2G and Gaia fit together. And shows how the system startup and the app launching process.",
+    },
+    {
       "title": "Building Games for Firefox OS",
       "author": "Andre Alves Garzia",
       "twitter": "soapdog",
